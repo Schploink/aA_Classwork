ca8b6740ad4491e85ccae2ed898155d8f94ba8f4
Schploink <thekpoc@gmail.com>
2021-04-06 15:17:11 -0700

    phase1 complete

--- oopexercise/chess/board.rb
+++ oopexercise/chess/board.rb
@@ -1,21 +1,20 @@
+require_relative "piece.rb"
+
 class Board
 
     def initialize
-        @null_piece = NullPiece.instance
+        # @null_piece = NullPiece.instance
         @rows = Array.new(8) {Array.new(8)}
+        populate_board
     end
 
     def populate_board
         @rows.each_with_index do |row, idx|
-
-            if idx.between(0,1) || idx.between(6,7)
-
+            if idx.between?(0,1) || idx.between?(6,7)
                 row.each_with_index do |spot, idx2|
-                    self[idx][idx2] = Piece.new(:b, 1, [idx, idx2])
+                    @rows[idx][idx2] = Piece.new(:b, 1, [idx, idx2])
                 end
-
             end
-
         end
     end
 
@@ -30,6 +29,18 @@ class Board
     end
 
     def move_piece(start_pos, end_pos)
+        if self[start_pos] == nil
+            raise "No piece there"
+        end
+        if !end_pos[0].between?(0, 7) || !end_pos[1].between?(0, 7)
+            raise "Where you goin"
+        end
+        piece = self[start_pos]
+        self[start_pos] = nil
+        self[end_pos] = piece
+        piece.pos = end_pos
 
     end
+    # should raise error if no piece at start pos
+    # error if piece cannot move to end_pos - if off board
 end
