5c1a9568b87eca608af359643b76df45a9c63b47
Juan Barba <barbajuan11@gmail.com>
2021-04-07 15:01:26 -0700

    Add cursor and display classes

---
+++ oopexercise/chess/cursor.rb
@@ -0,0 +1,87 @@
+require "io/console"
+
+KEYMAP = {
+  " " => :space,
+  "h" => :left,
+  "j" => :down,
+  "k" => :up,
+  "l" => :right,
+  "w" => :up,
+  "a" => :left,
+  "s" => :down,
+  "d" => :right,
+  "\t" => :tab,
+  "\r" => :return,
+  "\n" => :newline,
+  "\e" => :escape,
+  "\e[A" => :up,
+  "\e[B" => :down,
+  "\e[C" => :right,
+  "\e[D" => :left,
+  "\177" => :backspace,
+  "\004" => :delete,
+  "\u0003" => :ctrl_c,
+}
+
+MOVES = {
+  left: [0, -1],
+  right: [0, 1],
+  up: [-1, 0],
+  down: [1, 0]
+}
+
+class Cursor
+
+  attr_reader :cursor_pos, :board
+
+  def initialize(cursor_pos, board)
+    @cursor_pos = cursor_pos
+    @board = board
+  end
+
+  def get_input
+    key = KEYMAP[read_char]
+    handle_key(key)
+  end
+
+  private
+
+  def read_char
+    STDIN.echo = false # stops the console from printing return values
+
+    STDIN.raw! # in raw mode data is given as is to the program--the system
+                 # doesn't preprocess special characters such as control-c
+
+    input = STDIN.getc.chr # STDIN.getc reads a one-character string as a
+                             # numeric keycode. chr returns a string of the
+                             # character represented by the keycode.
+                             # (e.g. 65.chr => "A")
+
+    if input == "\e" then
+      input << STDIN.read_nonblock(3) rescue nil # read_nonblock(maxlen) reads
+                                                   # at most maxlen bytes from a
+                                                   # data stream; it's nonblocking,
+                                                   # meaning the method executes
+                                                   # asynchronously; it raises an
+                                                   # error if no data is available,
+                                                   # hence the need for rescue
+
+      input << STDIN.read_nonblock(2) rescue nil
+    end
+
+    STDIN.echo = true # the console prints return values again
+    STDIN.cooked! # the opposite of raw mode :)
+
+    return input
+  end
+  #depending on key it will return @cursor_pos in the case of return or space
+  #call the update_pos with the move difference from MOVES and return nil in the case of [up left right down]
+  #or it will exit from the terminal process in the case of ctrl_c
+  #exit a termial process use Process.exit pass it the status code 0 as an arguement
+  def handle_key(key)
+    case KEYMAP
+  end
+
+  def update_pos(diff)
+  end
+end
