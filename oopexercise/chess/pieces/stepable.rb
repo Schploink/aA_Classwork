3d7679513f85402e2b01b72bea177364697a9780
Juan Barba <barbajuan11@gmail.com>
2021-04-07 11:31:00 -0700

    Create stepable

---
+++ oopexercise/chess/pieces/stepable.rb
@@ -0,0 +1,28 @@
+module Stepable
+
+    HORIZONTAL_DIRS = [[0,1],[0,-1], [1,0], [-1,0]]
+    DIAGONAL_DIRS = [[1,1], [-1,1], [1,-1], [-1,-1]]
+    KNIGHT_MOVES = [[2,1],[2,-1],[-2,1],[-2,-1],[1,2],[1,-2],[-1,2],[-1,-2]]
+
+    def moves
+        potential_moves = []
+        ox, oy = @pos
+
+        move_diffs.each do |pos|
+            x, y = pos
+            cx, cy = ox+x, oy+y
+            if !cx.between?(0,7) || !cy.between?(0,7)
+                next
+            elsif @board[[cx,cy]].color == @board[[ox,oy]].color
+                next
+            end
+            potential_moves << [cx,cy]
+        end
+        potential_moves
+    end
+
+    private
+    def move_diffs
+    end
+
+end
