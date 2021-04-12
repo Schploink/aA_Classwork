c5b9e7b009acabad055c91f0d96f7c1abd3cb0b1
Schploink <thekpoc@gmail.com>
2021-04-12 16:59:18 -0700

    finish part 3

--- LRU_n_Hashmap/skeleton/lib/p03_hash_set.rb
+++ LRU_n_Hashmap/skeleton/lib/p03_hash_set.rb
@@ -7,18 +7,28 @@ class HashSet
   end
 
   def insert(key)
+    resize! if @count == num_buckets
+    unless include?(key)
+      self[key] << key
+      @count += 1
+    end
   end
 
   def include?(key)
+    self[key].include?(key)
   end
 
   def remove(key)
+    if include?(key)
+      self[key].delete(key)
+      @count -= 1
+    end
   end
 
   private
 
-  def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+  def [](key)
+    @store[key.hash % num_buckets]
   end
 
   def num_buckets
@@ -26,5 +36,13 @@ class HashSet
   end
 
   def resize!
+    @store.concat(Array.new(num_buckets) {Array.new})
+    (0...num_buckets/2).each do |i|
+      @store[i].each do |ele|
+        @store[i].delete(ele)
+        @count -= 1
+        insert(ele)
+      end
+    end
   end
 end
