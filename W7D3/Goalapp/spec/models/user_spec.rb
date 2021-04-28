9b7bc149c055b2091b911dc76ecf80a654f9d1b4
Ying <cy240705@gmail.com>
2021-04-28 12:22:44 -0700

    create spec is_password

--- W7D3/Goalapp/spec/models/user_spec.rb
+++ W7D3/Goalapp/spec/models/user_spec.rb
@@ -8,4 +8,13 @@ RSpec.describe User, type: :model do
 
   subject (:user1) {User.create(username: "Capy", password: "123456")}
   it { should validate_uniqueness_of(:username) }
+
+
+  describe "#is_password?" do
+    it "check if user exist with given password" do 
+      context "when password matches with given password"
+      expect(FacotryBot.create(:user).is_password?("123456")).to be(true)
+    end  
+
+  end 
 end
