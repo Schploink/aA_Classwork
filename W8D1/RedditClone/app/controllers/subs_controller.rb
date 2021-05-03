16f812998bb42e1a9fefea6e44728a563fdea944
Schploink <thekpoc@gmail.com>
2021-05-03 12:16:33 -0700

    fill out subs controller

--- W8D1/RedditClone/app/controllers/subs_controller.rb
+++ W8D1/RedditClone/app/controllers/subs_controller.rb
@@ -1,2 +1,48 @@
 class SubsController < ApplicationController
+
+  def index
+    @subs = Sub.all
+    render :index
+  end
+
+  def show
+    @sub = Sub.find(params[:id])
+    render :show
+  end
+
+  def new
+    @sub = Sub.new
+    render :new
+  end
+
+  def create
+    @sub = Sub.new(sub_params)
+    @sub.moderator_id = params[:moderator_id]
+    if @sub.save
+    else
+      flash[:errors] = @sub.errors.full_messages
+    end
+    redirect_to user_url(@sub.moderator_id)
+
+  end
+
+  def edit
+    @sub = Sub.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @sub = Sub.find_by(id: params[:id])
+
+    if @sub && @sub.update(sub_params)
+      redirect_to users_url
+    else
+      flash.now[:errors] = @sub.errors.full_messages
+      render :edit
+    end
+  end
+
+  def sub_params
+    params.require(:sub).permit(:title, :description)
+  end
 end
