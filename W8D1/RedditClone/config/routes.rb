4a2298ae5416538aa3291c3007ddca071c560c89
guocong <ryp0313@gmail.com>
2021-05-03 11:46:55 -0700

    progress

--- W8D1/RedditClone/config/routes.rb
+++ W8D1/RedditClone/config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resource :session, only: [:create, :destroy, :new]
+  resources :users, only: [:create, :new, :index, :show, :update, :edit] do 
+    resources :posts, only: [:edit, :update]
+  end
+  resources :subs
+  resources :posts, only: [:create, :new, :show, :update, :edit, :destroy]
+  resources :posts do
+    resources :users, only: [:edit, :update]
+  end
 end
