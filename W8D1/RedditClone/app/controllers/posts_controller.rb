80660e520128ee09395323a256aa5105ca0400be
guocong <ryp0313@gmail.com>
2021-05-03 14:14:11 -0700

    progress

--- W8D1/RedditClone/app/controllers/posts_controller.rb
+++ W8D1/RedditClone/app/controllers/posts_controller.rb
@@ -1,2 +1,58 @@
 class PostsController < ApplicationController
+
+    def new
+        @post = Post.new
+        render :new
+    end
+
+    def create
+        post = Post.new(post_params)
+        post.sub_id = params[:sub_id]
+        post.author_id = params[:author_id]
+        if post.save
+        else
+            flash[:errors] = post.errors.full_messages
+        end
+        redirect_to sub_url(params[:sub_id])
+    end
+
+    def show
+        @post = Post.find(params[:id])
+        render :show
+    end
+
+    def edit
+        @post = Post.find(params[:id])
+        if @post.author_id == current_user.id
+            render :edit
+        else
+            redirect_to subs_url
+        end
+    end
+
+    def update
+        post = Post.find_by(id: params[:id])
+
+        if post.author_id == current_user.id
+            if post.update(post_params)
+                redirect_to sub_url(params[:sub_id])
+            else
+                flash[:errors] = post.errors.full_messages
+                render :edit
+            end 
+        else
+            redirect_to subs_url
+        end
+    end
+
+    def destroy
+        post = Post.find_by(id: params[:id])
+        post.destroy
+        redirect_to subs_url
+    end
+
+    def post_params
+        params.require(:post).permit(:title, :url, :content)
+    end
+
 end
--- W8D1/RedditClone/config/routes.rb
+++ W8D1/RedditClone/config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resource :session, only: [:create, :destroy, :new]
-  resources :users, only: [:create, :new, :index, :show, :update, :edit] do 
-    resources :posts, only: [:edit, :update]
+  resources :users, only: [:create, :new, :index, :show, :update, :edit] 
+
+  resources :subs do
+    resources :posts, only: [:new, :create]
   end
-  resources :subs
-  resources :posts, only: [:create, :new, :show, :update, :edit, :destroy]
-  resources :posts do
-    resources :users, only: [:edit, :update]
-  end
+  resources :posts, only: [:show, :update, :edit, :destroy]
+
 end
